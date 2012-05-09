fa40b8473af3fb966b9b23876021b674f50c04e9
Paul Cichonski <paul.cichonski@gmail.com>
2012-05-09 17:04:28 -0400

    initial code that reads in an OWL RDF/XML file and spits out a map of classes and associated predicates. This is a very simple parse; much more intelligence needs to be added.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject owl-util "0.0.1-SNAPSHOT"
+  :description "Simple utilities for reading/writing OWL models"
+  :dependencies [
+                 [org.clojure/clojure "1.3.0"]
+                 ]
+  :dev-dependencies [
+                     [com.github.robertrolandorg/lein-eclipse "1.1.0"]                     
+                     ]    
+            )
---
+++ test/owl_util/test/reader/owlXmlReader.clj
@@ -0,0 +1,60 @@
+(ns owl-util.test.reader.owlXmlReader
+  (:use [owl-util.reader.owlXmlReader]
+        [clojure.test])
+  (:require [owl-util.reader.core :as reader-core]
+            [clojure.contrib.zip-filter.xml :as zip-xml])) 
+
+(def file-loc "test/owl_util/test/data/test-usecase.owl")
+
+(deftest test-classes-and-relationships
+  "test the classes-and-relationships function in owlXmlReader"
+  (let [ont (reader-core/classes-and-relationships file-loc)
+        classes-and-rels (ont :model)
+        namespaces (ont :namespaces)
+        classes (map #(% :class) classes-and-rels)]
+    ;; test namespace data
+    (is (= 6 (count namespaces)))
+    (is (= (namespaces :rdf) "http://www.w3.org/1999/02/22-rdf-syntax-ns#"))
+    (is (= (namespaces :owl) "http://www.w3.org/2002/07/owl#"))
+    (is (= (namespaces (keyword "")) "http://scap.nist.gov/vocab/test-usecase#"))
+    (is (= (namespaces :xsd) "http://www.w3.org/2001/XMLSchema#"))
+    (is (= (namespaces :rdfs) "http://www.w3.org/2000/01/rdf-schema#"))
+    (is (= (namespaces :mapping) "http://nist.gov/secaut/vocab/mapping#"))
+    ;; test basic class data
+    (is (= 3 (count classes-and-rels)))
+    (is (every? #{"#Asset" "#ProductClass" "#Checklist"} classes))
+    ;; test asset class and rels
+    (let [asset (first (filter #(= (% :class) "#Asset") classes-and-rels))
+          preds (asset :preds)]
+      (let [hasProductClass (first (filter #(= (% :id) "hasProductClass") preds))]
+        (is (= :owl:ObjectProperty (hasProductClass :type)))
+        (is (= "#Asset" (first (hasProductClass :domain))))
+        (is (= "#ProductClass" (first (hasProductClass :range))))))
+    ;; test product class and rels
+    (let [productClass (first (filter #(= (% :class) "#ProductClass") classes-and-rels))
+          preds (productClass :preds)]
+      (let [hasCpeName (first (filter #(= (% :id) "hasCpeName") preds))]
+        (is (= :owl:DatatypeProperty (hasCpeName :type)))
+        (is (= "#ProductClass" (first (hasCpeName :domain))))
+        (is (= "http://www.w3.org/2001/XMLSchema#string" (first (hasCpeName :range))))))
+    ;; test checklist class and rels
+    (let [checklist (first (filter #(= (% :class) "#Checklist") classes-and-rels))
+          preds (checklist :preds)]
+      (let [hasChecklistName (first (filter #(= (% :id) "hasChecklistName") preds))
+            hasChecklistId (first (filter #(= (% :id) "hasChecklistId") preds))
+            securesProduct (first (filter #(= (% :id) "securesProduct") preds))]
+        ;; hasChecklistName tests
+        (is (= :owl:DatatypeProperty (hasChecklistName :type)))
+        (is (= "#Checklist" (first (hasChecklistName :domain))))
+        (is (= "http://www.w3.org/2001/XMLSchema#string" (first (hasChecklistName :range))))
+        ;; hasChecklistId tests
+        (is (= :owl:DatatypeProperty (hasChecklistId :type)))
+        (is (= "#Checklist" (first (hasChecklistId :domain))))
+        (is (= "http://www.w3.org/2001/XMLSchema#integer" (first (hasChecklistId :range))))   
+        ;; securesProduct tests
+        (is (= :owl:ObjectProperty (securesProduct :type)))
+        (is (= "#Checklist" (first (securesProduct :domain))))
+        (is (= "#ProductClass" (first (securesProduct :range))))))))
+
+
+
