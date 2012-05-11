edacf4150f2b8a0fc7db8834154da3d62949bb65
Paul Cichonski <paul.cichonski@gmail.com>
2012-05-10 22:33:19 -0400

    initial code to write OWL, very vey basic and buggy. gen-rdf-xml is causing issues b/c it builds a lazy sequence for the 'elements', which emit cannot handle. May want to use something other than data.xml b/c it seems a bit buggy

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.xml "0.0.3"]
                  ]
   :dev-dependencies [
                      [com.github.robertrolandorg/lein-eclipse "1.1.0"]                     
